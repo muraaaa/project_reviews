3a5b175803fd6cc1814b879fd215358d960f9b6d
tm <tm@tms-MacBook-Air.local>
2019-09-22 16:56:19 +0900

    add:serchArea

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
- <div>トップページ
- </div>
+  <div>
+    <p class="hoge">トップページ</p>
+    <searchArea/>
+   </div>
 </template>
 
 <script>
-
+import searchArea from "@/components/searchArea";
 export default {
   name: 'app',
+  components: {
+    searchArea,
+  },
 }
 </script>
 
 <style lang="scss">
-  @import './assets/style/global/_reset';
-  @import './assets/style/global/_common';
+@import './assets/style/global/_reset';
+@import './assets/style/global/_common';
+.sectionTitle {
+  color: white;
+  background-color: blue;
+}
 </style>
---
+++ src/components/searchArea.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <p class="sectionTitle" >
+      このコンポーネントはseachareaです
+    </p>
+    <img src="" >
+    <li v-for="item in listItems">
+      <a :class="'wordList__item__link--' + item.iconType">{{ item.name }}</a>
+    </li>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listItems: [
+        { name: "Java", iconType: "code" },
+        { name: "PHP", iconType: "code" },
+        { name: "Python", iconType: "laptopCode" },
+        { name: "JavaScript", iconType: "laptopCode" },
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.hoge {
+  color: red;
+}
+</style>
